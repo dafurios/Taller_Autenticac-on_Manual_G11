da539ad7b97c95c9672c0eed072b16ef5b1609df
dafurios <dafurios@gmail.com>
2018-02-23 11:53:20 -0300

    primer commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'Has cerrado sesión con éxito'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   resources :histories
 
+  resources :sessions, only: [:create, :destroy]
+
+  get 'users/sign_up', to: "users#new"
+  post 'users', to: "users#create"
+
+  get 'users/sign_in', to: "session#new"
+  post 'users', to: "users#create"
+
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
